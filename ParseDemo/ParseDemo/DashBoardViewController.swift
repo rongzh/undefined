193096f76aba94534fac84b89f9045a0b72822c5
Yuting Wu <wuyuting@umich.edu>
2015-12-10 16:31:02 -0500

    continue fixing....
    continue fixing....

--- ParseDemo/ParseDemo/DashBoardViewController.swift
+++ ParseDemo/ParseDemo/DashBoardViewController.swift
@@ -15,7 +15,6 @@ class DashBoardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.usernameLabel.text = PFUser.currentUser()!.username
         // Show the current visitor's username
     }
     
